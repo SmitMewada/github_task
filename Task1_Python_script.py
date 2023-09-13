acbb720e208b881e7d70dfedb00d26e8a941f07f
Smit Mewada <smitmewada009@gmail.com>
2023-09-13 13:20:44 -0400

    made a pull request

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -70,3 +70,6 @@ if __name__ == '__main__':
 	save_file(text)
 	# Instructions to Run this python code
 	# Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
+
+
+# Made a pull request
